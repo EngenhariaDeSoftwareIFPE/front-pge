e4ba48fa844b0afa5b9dbdcf66e34f093d5d6f2a
unknown <cgti@NOTE-MT19.belojardim.ifpe>
2023-11-13 14:22:08 -0300

    feat:Finalização Navbar Horizontal

--- src/components/navBar/navHorizontal/NavBarHorizontal.jsx
+++ src/components/navBar/navHorizontal/NavBarHorizontal.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
-import Image from 'next/image'
 
-import pgeLogo from "./../../../../public/pge_logo.png"
-
-import Profile from './Profile'
+import Profile from '../../ui/Profile'
 
 const NavBarHorizontal = () => {
   return (
     <div className='h-full'>
         <div className="w-full h-28 bg-green-900 flex justify-between items-center">
-            <Image
-                src={pgeLogo}
-                alt="Logo do portal"
-                className='h-full'
-            />
+            <img src="pge_logo.png" alt="Logo do portal" className='w-40 ms-5' />
             <Profile/>
         </div>
     </div>
