673ab5ce616e1a47d95b2ca76539799838932f7d
luizcarlosom <lcom@discente.ifpe.edu.br>
2023-11-22 08:32:44 -0300

    fix: Adjusted heights

--- src/app/password_reset/page.jsx
+++ src/app/password_reset/page.jsx
@@ -1,6 +1,11 @@
 import BackgroundAuth from "@/components/BackgroundAuth";
 import FormForgotPasswordStep1 from "@/components/FormForgotPasswordStep1";
 
+export const metadata = {
+    title: 'PGE | Recuperação de senha',
+    description: 'Login PGE',
+}
+
 export default function PasswordReset() {
     const components = [
         <FormForgotPasswordStep1/>
