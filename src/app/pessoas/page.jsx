ac65cc88d24c0861480fee160ef4247cf567ed3a
NOTE-MT19\cgti <guilhermelyare@gmail.com>
2023-12-04 16:50:45 -0300

    fix: Refactor peploe screen

--- src/app/pessoas/page.jsx
+++ src/app/pessoas/page.jsx
@@ -11,7 +11,13 @@ export default function Pessoas() {
   return (
         <>
             <div className="w-screen h-screen font-monteserrat">
-                <NavBarHorizontal/>
+                <NavBarHorizontal 
+                    elementos={[
+                    { title: 'Ver conteúdo', id: 'conteudo' },
+                    { title: 'Ir para menu', id: 'menu' },
+                    { title: 'Ir para perfil', id: 'perfil' }, 
+                    ]}
+                />
                 <div className="flex flex-row">
                     <NavBarVertical/>
                     <HandlePeople />
